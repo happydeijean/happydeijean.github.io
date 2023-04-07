070d308982628c15e2a2247c631ac6391761adf8
Donny M <nomok67810@gmail.com>
2023-04-07 02:17:12 -0500

    fetch url fix

--- fnaftitlegen/js/titles.js
+++ fnaftitlegen/js/titles.js
@@ -6,7 +6,7 @@ import data from '/fnaftitlegen/titles.json' assert {type: 'json'};
 */
 
 async function returnTitles(){
-	return await fetch("https://raw.githubusercontent.com/Nomok/nomok.github.io/main/fnaftitlegen/titles.json")
+	return await fetch("https://raw.githubusercontent.com/Nomok/fnaftitlegen/main/titles.json")
 	.then(response => response.json())
 	.then(data => {
 		return data;
